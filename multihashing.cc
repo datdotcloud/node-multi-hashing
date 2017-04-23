23af6be12123336cdcfdd0089768d78145ecc279
DeffM <b-centr@mail.ru>
2017-04-23 13:19:18 +0700

    Add files via upload

--- multihashing.cc
+++ multihashing.cc
@@ -33,6 +33,8 @@ extern "C" {
     #include "x5.h"
     #include "c11.h"
     #include "thriddle.h"
+    #include "lyra2.h"
+    #include "timetravel.h"
 }
 
 #include "boolberry.h"
@@ -777,6 +779,45 @@ Handle<Value> thriddle(const Arguments& args) {
     return scope.Close(buff->handle_);
 }
 
+Handle<Value> lyra2(const Arguments& args) {
+    HandleScope scope;
+
+    if (args.Length() < 1)
+        return except("You must provide one argument.");
+
+    Local<Object> target = args[0]->ToObject();
+
+    if(!Buffer::HasInstance(target))
+        return except("Argument should be a buffer object.");
+
+    char output[32];
+
+    Buffer* buff = Buffer::New(output, 32);
+    return scope.Close(buff->handle_);
+}
+
+Handle<Value> timetravel(const Arguments& args) {
+    HandleScope scope;
+
+    if (args.Length() < 1)
+        return except("You must provide one argument.");
+
+    Local<Object> target = args[0]->ToObject();
+
+    if(!Buffer::HasInstance(target))
+        return except("Argument should be a buffer object.");
+
+    char * input = Buffer::Data(target);
+    char output[32];
+
+    uint32_t input_len = Buffer::Length(target);
+
+    timetravel_hash(input, output, input_len);
+
+    Buffer* buff = Buffer::New(output, 32);
+    return scope.Close(buff->handle_);
+}
+
 void init(Handle<Object> exports) {
     exports->Set(String::NewSymbol("quark"), FunctionTemplate::New(quark)->GetFunction());
     exports->Set(String::NewSymbol("x11"), FunctionTemplate::New(x11)->GetFunction());
@@ -808,6 +849,8 @@ void init(Handle<Object> exports) {
     exports->Set(String::NewSymbol("jh"), FunctionTemplate::New(jh)->GetFunction());
     exports->Set(String::NewSymbol("c11"), FunctionTemplate::New(c11)->GetFunction());
     exports->Set(String::NewSymbol("thriddle"), FunctionTemplate::New(thriddle)->GetFunction());
+    exports->Set(String::NewSymbol("lyra2"), FunctionTemplate::New(lyra2)->GetFunction());
+    exports->Set(String::NewSymbol("timetravel"), FunctionTemplate::New(timetravel)->GetFunction());
 }
 
 NODE_MODULE(multihashing, init)
